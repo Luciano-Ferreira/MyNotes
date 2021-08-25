cce2ee17f82770d9b0de6e2d76a78c40e8f1a77b
Luciano Ferreira <luciano2023silva@gmail.com>
2021-08-25 18:19:17 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <img src="/images/Logo.png" alt="Space Traveling" />
+      </header>
+    </>
+  );
 }
